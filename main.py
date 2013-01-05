5b889928345d207de3242c65585b7ce2d6f985cb
Alex <theworldcreator@gmail.com>
2013-01-05 20:50:32 +0400

    Crawler and better parsing

--- main.py
+++ main.py
@@ -6,63 +6,111 @@ from lxml.html.clean import Cleaner
 import urllib
 import os
 import shutil
+from urlparse import urlparse
 
 tmp_folder = 'tmp/'
 
-base_url = "http://ru.fcknvermodes.wikia.com/wiki/%D0%A2%D0%B5%D0%BE%D1%80%D0%B8%D1%8F_"
-url_list = []
-for k in range(1, 2):
-    url_list.append(base_url + str(k))
+base_url = "http://ru.fcknvermodes.wikia.com/wiki/"
+
+
+def unique(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if x not in seen and not seen_add(x)]
 
 
 def html2pdf(html, pdf):
-    print 'wkhtmltopdf ' + html + ' ' + pdf
     return os.system('wkhtmltopdf ' + html + ' ' + pdf)
+
+
+def clean_all_but(tree, id_to_keep):
+    # Clean all tags but with id_to_keep id
+    # Also leave allowed_tags
+
+    allowed_tags = ["script"]
+    for child in tree:
+        if not (len(child.cssselect('#' + id_to_keep)) > 0 or child.tag in allowed_tags):
+            child.drop_tree()
+
+
+def clean_controls(root):
+    # Clean all control elements form Wikia page
+    denied_classes = ['wikia-menu-button', 'comments',
+                    'WikiaArticleCategories', 'editsection',
+                    'printfooter']
+    for cl in denied_classes:
+        elements = root.cssselect('.' + cl)
+        for el in elements:
+            el.drop_tree()
+
+
+def parent_in_tag(element, tag):
+    # Find parent of element which is direct child of tag
+    parent = element
+    while parent.getparent().tag != tag:
+        parent = element.getparent()
+    return parent
+
+
+def all_internal_links(url):
+    # Get all intenal links from the page
+
+    o = urlparse(url)
+    base_loc = o.netloc
+    page = urllib.urlopen(url)
+    doc = lxml.html.document_fromstring(page.read())
+    page.close()
+
+    content = doc.cssselect('body')[0].getchildren()
+    clean_all_but(content, 'WikiaPage')
+    article = doc.cssselect('div.WikiaPageContentWrapper')[0].getchildren()
+    clean_all_but(article, 'WikiaMainContent')
+
+    clean_controls(doc)
+    links_obj = doc.cssselect('a')
+    links = []
+    for obj in links_obj:
+        curr_url = obj.get('href')
+        o = urlparse(curr_url)
+        if o.netloc == '':
+            if curr_url[0] != '/':
+                curr_url = '/' + curr_url
+            curr_url = 'http://' + base_loc + curr_url
+            o = urlparse(curr_url)
+        if o.netloc == base_loc and o.fragment == '' and o.query == '' and ':' not in o.path:
+            links.append(curr_url)
+            # print curr_url
+    return unique(links)
 
 if os.path.exists(tmp_folder):
     shutil.rmtree(tmp_folder)
 os.makedirs(tmp_folder)
 
+url_list = all_internal_links(base_url)
+print url_list
 i = 1
-cleaner = Cleaner(scripts=False)
+cleaner = Cleaner(comments=True, page_structure=False, scripts=False)
 pdf_list = []
 for url in url_list:
-    print url
     page = urllib.urlopen(url)
     doc = lxml.html.document_fromstring(page.read())
-    content = doc.xpath('/html/body/')
-    for child in content:
-        print child.tag
-        if child.get('id') != 'WikiaPage':
-            child.drop_tree()
-    article = doc.cssselect('#WikiaPage')
-    for child in article:
-        if child.get('id') != 'WikiaMainContent':
-            child.drop_tree()
-    f = open('test.html', 'a')
-    f.write(cleaner.clean_html(lxml.html.tostring(doc)))
-    # article = doc.cssselect('.mw-headline')
-    # for curr in article:
-    #     html_filename = tmp_folder + str(i) + '.html'
-    #     pdf_filename = tmp_folder + str(i) + '.pdf'
-    #     f = open(html_filename, 'a')
-    #     f.write('<meta http-equiv="Content-Type" content="text/html; charset=utf-8">')
-    #     header = cleaner.clean_html(lxml.html.tostring(curr))
-    #     f.write('<h2>' + header + '</h2>')
+    page.close()
+    html_filename = tmp_folder + str(i) + '.html'
+    pdf_filename = tmp_folder + str(i) + '.pdf'
+    content = doc.cssselect('body')[0].getchildren()
+    clean_all_but(content, 'WikiaPage')
+    # article = doc.cssselect('div.WikiaPageContentWrapper')[0].getchildren()
+    # clean_all_but(article, 'WikiaMainContent')
 
-    #     curr = curr.getparent().getnext()
-    #     while curr != None and curr.tag != 'h2' and (len(curr.cssselect('.mw-redirect')) == 0 or curr.cssselect('.mw-redirect')[0].text_content() != u'Предыдущий билет'):
-    #         try:
-    #             html = cleaner.clean_html(lxml.html.tostring(curr))
-    #         except:
-    #             pass
-    #         f.write(html)
-    #         curr = curr.getnext()
-    #     f.close()
+    clean_controls(doc)
 
-    #     html2pdf(html_filename, pdf_filename)
-    #     pdf_list.append(pdf_filename)
-    #     i = i + 1
+    f = open(html_filename, 'w')
+    f.write(lxml.html.tostring(doc))
+    f.close()
 
-# os.system('pdfjoin ' + ' '.join(pdf_list) + ' --outfile output.pdf')
-# shutil.rmtree(tmp_folder)
+    html2pdf(html_filename, pdf_filename)
+    pdf_list.append(pdf_filename)
+    i = i + 1
+
+os.system('pdfjoin ' + ' '.join(pdf_list) + ' --outfile output.pdf')
+shutil.rmtree(tmp_folder)
